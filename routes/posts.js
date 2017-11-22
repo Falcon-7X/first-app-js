d1003c08021aad1cd69c833d081c7dcf233317e3
fengkaiwater <328458211@qq.com>
2017-11-22 09:41:16 +0800

    add vue and axios methods to posts

--- routes/posts.js
+++ routes/posts.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET posts page. */
 router.get('/', function(req, res, next) {
-  res.render('posts', { title: 'My First Post' });
+  res.render('posts', { title: 'posts', postsList: ['文章1', '文章2', '文章3']});
+});
+
+/* GET 获取postsList数据 */
+router.get('/list', function(req, res, next) {
+  res.json({postsList: ['文章1', '文章2', '文章3'] });
 });
 
 module.exports = router;
